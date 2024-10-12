99cd8417ecdcca8fbe24d0f3d09a7eadd6ce7a6e
TuanDatDeverloper <datnst.24it@vku.udn.vn>
2024-10-12 22:51:00 +0700

    first commit

--- baitaphangngay/baitap4.cpp
+++ baitaphangngay/baitap4.cpp
@@ -2,12 +2,25 @@
 using namespace std;
 int main(){
     int t,n;
-    cin >> t>>n;
-    if((t>0)&&(n>0)){
-        cout << t<<" "<<n<< endl;
-    }
-    else{
-        cout << "NO"<<endl;
+    cin >> t;
+    cin >> n;
+    if((t<=0)||(t>12)||(n<=0)){
         cout << "INVALID"<<endl;
     }
+    else if((t==1)||(t==3)||(t==5)||(t==7)||(t==8)||(t==10)||(t==12)){
+        cout << "31"<<endl;
+    }
+    else if((t==4)||(t==6)||(t==9)||(t==11)){
+        cout << "30"<<endl;
+    }
+    else if((t=2)&&(n%4==0)&&(n%100!=0)||(n%400==0)){
+        cout << "29"<<endl;
+    }
+    else if((t=2)&&(n%4!=0)){
+        cout << "28"<<endl;
+    }
+    
+        
+        
+    
 }
