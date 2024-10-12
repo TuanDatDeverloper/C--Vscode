6a39b5641466212e1b63ddd4ccc3fa49aef93cdd
TuanDatDeverloper <datnst.24it@vku.udn.vn>
2024-10-12 22:27:23 +0700

    bài luyên số 4

---
+++ baitaphangngay/baitap4.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t,n;
+    cin >> t>>n;
+    if((t>0)&&(n>0)){
+        cout << t<<" "<<n<< endl;
+    }
+    else{
+        cout << "NO"<<endl;
+        cout << "INVALID"<<endl;
+    }
+}
