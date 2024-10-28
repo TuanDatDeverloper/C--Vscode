2f14679758dbfcc8cc933c2b50f234d3aaa093eb
TuanDatDeverloper <datnst.24it@vku.udn.vn>
2024-10-29 00:19:37 +0700

    first commit

--- baitaphangngay/baitap53.cpp
+++ baitaphangngay/baitap53.cpp
@@ -1,17 +1,71 @@
-#include <iostream>
+/*
+Cho một xâu con độ dài n. Hãy tìm cách thay thế nhiều nhất 2 kí tự để thu được 1 xâu đối xứng.
+Input
+Gồm T testcase (T ≤ 10), mỗi testcase nằm trên một dòng:
+  Mỗi dòng gồm 1 xâu s (∣s∣ ≤ 600)
+Output
+  Hãy in ra T dòng, mỗi dòng là YES nếu có cách thực hiện yêu cầu trên, hoặc NO nếu không tồn tại cách nào.
+Example
+Input 
+zcxxxc
+xxczxx
+zxcvbn 
+Output
+YES
+YES
+NO
+Note
+Cách biến đổi từng testcase như sau:
+zcxxxc -> ccxxcc
+xxczxx -> xxccxx
+không có cách biến đổi thoả mãn
+*/
+// Lưu ý: Đề bài yêu cầu thay thế nhiều nhất 2 kí tự để thu được 1 xâu đối xứng
+// Để xâu đối xứng thì xâu đó phải có số kí tự chẵn và số kí tự xuất hiện của mỗi kí tự phải chẵn
+// Nếu xâu có số kí tự lẻ thì không thể tạo xâu đối xứng
+// Nếu xâu có số kí tự chẵn thì chỉ cần kiểm tra số lần xuất hiện của mỗi kí tự
+// Nếu có 2 kí tự xuất hiện lẻ thì không thể tạo xâu đối xứng
+// Nếu có 1 kí tự xuất hiện lẻ thì chỉ cần thay thế 1 kí tự đó để tạo xâu đối xứng
+// Nếu không có kí tự nào xuất hiện lẻ thì chỉ cần thay thế 2 kí tự bất kì để tạo xâu đối xứng
+#include <bits/stdc++.h>
+using namespace std;
+
+bool check(string s) {
+    int n = s.length();
+    int cnt[26] = {0};
+    for (int i = 0; i < n; i++) {
+        cnt[s[i] - 'a']++;
+    }
+    int odd = 0;
+    for (int i = 0; i < 26; i++) {
+        if (cnt[i] % 2 == 1) {
+            odd++;
+        }
+    }
+    if (n % 2 == 0) {
+        if (odd == 0) {
+            return true;
+        }
+        return false;
+    }
+    if (odd == 1) {
+        return true;
+    }
+    return false;
+}
 
 int main() {
-    long long sum = 0;
-    long long number;
-
-    while (true) {
-        std::cin >> number;
-        if (number == 0) {
-            break;
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        if (check(s)) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
         }
-        sum += number;
     }
-
-    std::cout << sum << std::endl;
     return 0;
 }
+
--- baitaphangngay/baitap56.cpp
+++ baitaphangngay/baitap56.cpp
@@ -1,58 +1,30 @@
-#include <iostream>
-#include <vector>
+/*
+Để sẵn sàng triển khai tiêm Vacxin cho địa phương có nguy cơ bùng dịch cao, người ta cần dự trữ không ít hơn n liều vacxin. Hiện nay trong kho đang có m liều vacxin, trong nước có hai cơ sở A,B sản xuất Vacxin. Nếu làm việc hết công suất, cơ sở A mỗi ngày sản xuất được pa liều, còn cơ sở B sản xuất được pb liều. Em hãy xác định sớm nhất sau bao nhiêu ngày sẽ có đủ n liều vacxin?
+Input
+Dòng đầu ghi hai số nguyên n,m(0 ≤ n,m ≤ 10^8)
+Dòng thứ hai chứa hai số nguyên pa,pb(0 ≤ pa,pb ≤ 10^5)
+Output
+  Ghi một số nguyên là số ngày sớm nhất có đủ vacxin dự trữ theo kế hoạch
+Example
+Input
+200 50 
+20 35 
+Ouput
+3
+*/
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    int n, k, d;
-    cin >> n >> k >> d;
+    int n, m, pa, pb;
+    cin >> n >> m >> pa >> pb;
 
-    vector<pair<int, int>> events(k);
-    for (int i = 0; i < k; ++i) {
-        cin >> events[i].first >> events[i].second;
+    int days = 0;
+    while (m < n) {
+        m += pa + pb;
+        days++;
     }
 
-    vector<int> p(n + 1);
-    for (int i = 1; i <= n; ++i) {
-        p[i] = i;
-    }
-
-   
-    for (int i = 1; i <= n; ++i) {
-        int pos = i;
-        for (const auto& event : events) {
-            int l = event.first, r = event.second;
-            if (l <= pos && pos <= r) {
-                pos = l + r - pos;
-            }
-        }
-        p[i] = pos;
-    }
-
-    
-    vector<bool> visited(n + 1, false);
-    vector<int> ans(n + 1);
-    for (int i = 1; i <= n; ++i) {
-        if (!visited[i]) {
-            vector<int> cycle;
-            int pos = i;
-            while (!visited[pos]) {
-                visited[pos] = true;
-                cycle.push_back(pos);
-                pos = p[pos];
-            }
-
-            int len = cycle.size();
-            for (int j = 0; j < len; ++j) {
-                int new_pos = (j + d) % len;
-                ans[cycle[new_pos]] = cycle[j];
-            }
-        }
-    }
-
-    for (int i = 1; i <= n; ++i) {
-        cout << ans[i] << " ";
-    }
-    cout << endl;
-
+    cout << days << endl;
     return 0;
 }
