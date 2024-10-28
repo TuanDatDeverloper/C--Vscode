a5473ec42e93511dd1ad450f5d9a8a4801bfec05
TuanDatDeverloper <datnst.24it@vku.udn.vn>
2024-10-29 00:19:51 +0700

    first commit

---
+++ baitaphangngay/test1.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    string s;
+    while(getline(cin, s)){
+        if(s.empty()){
+            cout << "YES\n";
+            continue;
+        }
+        int n = s.size();
+        int mismatches = 0;
+        for(int i=0; i < n/2; i++){
+            if(s[i] != s[n-1-i]){
+                mismatches++;
+            }
+        }
+        if(mismatches <= 2){
+            cout << "YES\n";
+        }
+        else{
+            cout << "NO\n";
+        }
+    }
+}
--- baitaphangngay/test2.cpp
+++ baitaphangngay/test2.cpp
@@ -1,60 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-const ll INF = 1e18;
-vector<ll> computeDP(const vector<pair<ll, ll>>& items, int excludeIndex, ll total_value) {
-    vector<ll> DP(total_value + 1, INF);
-    DP[0] = 0;
-    for (int i = 0; i < items.size(); i++) {
-        if (i == excludeIndex) continue; 
-        ll wi = items[i].first;
-        ll ci = items[i].second;
-        for (ll v = total_value; v >= ci; v--) {
-            DP[v] = min(DP[v], DP[v - ci] + wi);
-        }
-    }
-    return DP;
-}
-
-ll findMaxValue(const vector<ll>& DP, ll weight_limit) {
-    ll max_value = 0;
-    for (ll v = 0; v < DP.size(); v++) {
-        if (DP[v] <= weight_limit) {
-            max_value = max(max_value, v);
-        }
-    }
-    return max_value;
-}
-
-int main() {
+int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
-
-    int N;
-    ll W, H;
-    cin >> N >> W >> H;
-    vector<pair<ll, ll>> items(N);
-    ll total_value = 0;
-    for (int i = 0; i < N; i++) {
-        cin >> items[i].first >> items[i].second; 
-        total_value += items[i].second;
+    string s;
+    while(getline(cin, s)){
+        if(s.empty()){
+            cout << "YES\n";
+            continue;
+        }
+        int n = s.size();
+        int mismatches = 0;
+        for(int i=0; i < n/2; i++){
+            if(s[i] != s[n-1-i]){
+                mismatches++;
+            }
+        }
+        if(mismatches <= 2){
+            cout << "YES\n";
+        }
+        else{
+            cout << "NO\n";
+        }
     }
-
-    vector<ll> DP1 = computeDP(items, -1, total_value);
-    ll max_v1 = findMaxValue(DP1, W);
-
-    ll result = max_v1; 
-    for (int j = 0; j < N; j++) {
-        ll wj = items[j].first;
-        ll cj = items[j].second;
-        if (wj > H) continue; 
-        vector<ll> DPj = computeDP(items, j, total_value);
-        ll max_vj = findMaxValue(DPj, W);
-
-        ll total_val = max_vj + cj; 
-        result = max(result, total_val);
-    }
-
-    cout << result;
-}
+} 
